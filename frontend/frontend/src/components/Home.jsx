c1b06faa7301857f8d9c903bdc8af625c887fb01
Rudraksh Kumrawat <rudrakshkumrawat110@gmail.com>
2023-11-29 18:03:11 +0530

    added scholarship detail cards in home

--- frontend/frontend/src/components/Home.jsx
+++ frontend/frontend/src/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import ScholarshipDetail from './ScholarshipDetail'
+import './ScholarshipDetailCss.css'
+
 
 const Home = () => {
   return (
-    <div>
+    <div className='HomeContainer'>
       <Navbar />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
+      <ScholarshipDetail />
     </div>
   )
 }
---
+++ frontend/frontend/src/components/ScholarshipDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ScholarshipDetailCss.css'
+
+const ScholarshipDetail = () => {
+  return (
+      <div class="card">
+    <div class="card-image"></div>
+    <div class="category"> Illustration </div>
+    <div class="heading"> A heading that must span over two lines
+        <div class="author"><span class="name">Last Date :</span> 29-01-24</div>
+    </div>
+</div>
+  )
+}
+
+export default ScholarshipDetail;
