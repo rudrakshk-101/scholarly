d8401b603865cb8ca6f85461d86982754f91ffb5
Rudraksh Kumrawat <rudrakshkumrawat110@gmail.com>
2023-11-30 09:42:00 +0530

    added detail cards in home

--- frontend/frontend/src/components/ScholarshipDetail.jsx
+++ frontend/frontend/src/components/ScholarshipDetail.jsx
@@ -5,8 +5,8 @@ const ScholarshipDetail = () => {
   return (
       <div class="card">
     <div class="card-image"></div>
-    <div class="category"> Illustration </div>
-    <div class="heading"> A heading that must span over two lines
+    <div class="category"> Central Sector </div>
+    <div class="heading"> Scholarship provided to students of OBC caste.
         <div class="author"><span class="name">Last Date :</span> 29-01-24</div>
     </div>
 </div>
